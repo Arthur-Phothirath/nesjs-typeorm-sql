d2c7a4af220c665da2c2584fe8ed46c78d2e813a
Arthur-Phothirath <a_phothirath@etu-webschoolfactory.fr>
2023-03-24 16:36:33 +0100

    feat/pipe

--- src/users/dtos/CreateUserProfile.dto.ts
+++ src/users/dtos/CreateUserProfile.dto.ts
@@ -1,7 +1,21 @@
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
 export class CreateUserProfileDto {
+  @IsNumber()
   id: number;
+
+  @IsNotEmpty()
+  @IsString()
   firstName: string;
+
+  @IsNotEmpty()
+  @IsString()
   lastName: string;
+
+  @IsNumber()
   age: number;
+
+  @IsNotEmpty()
+  @IsString()
   dob: string;
 }
